7d45ff45a21c3d2f4d2bf8c2ddb7432ddea96148
chojuninengu <chojuninengu@gmail.com>
2024-10-25 11:56:44 +0100

    this displays the name of the user  alongside the date and time

---
+++ displayname.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Prompt user to enter their name
+echo "Enter your name:"
+read username
+
+# Get the current date and time
+date=$(date)
+
+# Print the personalized greeting
+echo "Hello welcome to you,mr9 salutes you, $username! Today is $date."
